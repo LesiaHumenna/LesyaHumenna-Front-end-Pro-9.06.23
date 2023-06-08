697cfcaa545123033b5bb4f388696178919e289f
Lesia Humenna <lgumenna61@gmail.com>
2023-06-08 23:20:11 +0200

    addes js-tasks

---
+++ Hillel-2-8.06.23/index.js
@@ -0,0 +1,32 @@
+//1
+for(let i = 20; i <= 30; i+= 0.5){
+    console.log(i)
+};
+
+//2
+for(let i = 0; i <= 40; i++){
+    if(i % 10 === 0){
+    console.log(i);
+    }
+};
+
+//3
+for(let i = 1; i <= 100; i++){
+    if(i * i <= 100){
+    console.log(i);
+    }  
+};
+
+//4
+const a = 35;
+    for(let i = 0; i <= 35; i++){
+        if(a > i && a % i === 0){
+        console.log(i);
+        }
+    };
+
+//5
+const b = 5;
+const c = 3;
+let result = b ** c;
+console.log(result);
